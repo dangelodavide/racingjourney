8d3b32b52e8c8329d90b89e8336f56e5e0204dcb
Davide D'Angelo <123898394+dangelodavide@users.noreply.github.com>
2025-01-15 16:44:50 +0100

    Add files via upload

---
+++ Racing Journey/frontend/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Homepage Gestionale</h1>
+      <p>Benvenuto nella tua Dashboard! Da qui puoi gestire le competizioni e gli allenamenti.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
